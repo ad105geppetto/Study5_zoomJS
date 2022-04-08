d4a9e5a7537d4b1e52dbc99f2e7f90981423e269
geppetto <ad105geppetto@gmail.com>
2022-04-08 20:57:38 +0900

    revision nicname

--- src/server.js
+++ src/server.js
@@ -30,8 +30,10 @@ wss.on("connection", (socket)=> {
         switch(message.type){
             case "new_message":
                 sockets.forEach((aSocket) => aSocket.send(`${socket.nickname} : ${message.payload}`));
+                break;
             case "nickname":
                 socket.nickname = message.payload;
+                break;
         }
     })
 })
