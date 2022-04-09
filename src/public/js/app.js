754f585cb76c5da4cbd2e2c98b1a824c7f91ac12
geppetto <ad105geppetto@gmail.com>
2022-04-09 22:07:27 +0900

    learning Socket.io

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,48 +1,64 @@
-const messageList = document.querySelector('ul');
-const nickForm = document.querySelector('#nick');
-const messageForm = document.querySelector('#message');
+const socket = io();
 
-const webSocket = new WebSocket(`ws://${window.location.host}`);
+const welcome = document.querySelector('#welcome');
+const form = document.querySelector('form');
 
-function makeMessage(type, payload) {
-    const msg = {type, payload};
-    return JSON.stringify(msg)
-}
-
-webSocket.addEventListener('open', () => {
-    console.log('connected to server')
-})
-
-webSocket.addEventListener('message', (message) => {
-    const li = document.createElement('li');
-    li.innerText = message.data;
-    messageList.append(li);
-})
-
-webSocket.addEventListener('close', () => {
-    console.log('disconnected from server')
-})
-
-// setTimeout(()=>{
-//     webSocket.send('hello from the browser')
-// }, 10000)
-
-const handleSubmit = (event) => {
+function handleRoomSubmit(event){
     event.preventDefault();
-    const input = messageForm.querySelector('input');
-    webSocket.send(makeMessage('new_message',input.value));
-    const li = document.createElement('li');
-    li.innerText = `You: ${message.data}`;
-    messageList.append(li);
-    input.value = ''
+    const input = document.querySelector('input');
+    socket.emit('enter_room', {payload: input.value}, () => {
+        console.log('server is done')
+    })
+    input.value = '';
 }
 
-const handleNickSubmit = (event) => {
-    event.preventDefault();
-    const input = nickForm.querySelector('input');
-    webSocket.send(makeMessage('nickname', input.value));
-    input.value = ''
-}
+form.addEventListener('submit', handleRoomSubmit)
 
-messageForm.addEventListener('submit', handleSubmit)
-nickForm.addEventListener('submit', handleNickSubmit)
+// const messageList = document.querySelector('ul');
+// const nickForm = document.querySelector('#nick');
+// const messageForm = document.querySelector('#message');
+
+// const webSocket = new WebSocket(`ws://${window.location.host}`);
+
+// function makeMessage(type, payload) {
+//     const msg = {type, payload};
+//     return JSON.stringify(msg)
+// }
+
+// webSocket.addEventListener('open', () => {
+//     console.log('connected to server')
+// })
+
+// webSocket.addEventListener('message', (message) => {
+//     const li = document.createElement('li');
+//     li.innerText = message.data;
+//     messageList.append(li);
+// })
+
+// webSocket.addEventListener('close', () => {
+//     console.log('disconnected from server')
+// })
+
+// // setTimeout(()=>{
+// //     webSocket.send('hello from the browser')
+// // }, 10000)
+
+// const handleSubmit = (event) => {
+//     event.preventDefault();
+//     const input = messageForm.querySelector('input');
+//     webSocket.send(makeMessage('new_message',input.value));
+//     const li = document.createElement('li');
+//     li.innerText = `You: ${message.data}`;
+//     messageList.append(li);
+//     input.value = ''
+// }
+
+// const handleNickSubmit = (event) => {
+//     event.preventDefault();
+//     const input = nickForm.querySelector('input');
+//     webSocket.send(makeMessage('nickname', input.value));
+//     input.value = ''
+// }
+
+// messageForm.addEventListener('submit', handleSubmit)
+// nickForm.addEventListener('submit', handleNickSubmit)
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
-import WebSocket from "ws";
+// import WebSocket from "ws";
+import SocketIO from "socket.io";
 import http from "http"
 
 const app = express();
@@ -10,37 +11,51 @@ app.set('views', __dirname + '/views');
 app.use('/public', express.static(__dirname + '/public'))
 app.get('/', (req, res) => res.render('home'))
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-const server = http.createServer(app); // express app에서부터 http서버를 만든다.
-const wss = new WebSocket.Server({server})  // http서버를 wss서버로 
+const httpServer = http.createServer(app);
+const wsServerio = SocketIO(httpServer);
+
+wsServerio.on('connection', socket => {
+    socket.on('enter_room', (msg, done) => {
+        console.log(msg);
+        setTimeout(() => {
+            done()
+        }, 10000)
+    })
+})
+
+httpServer.listen(3000, handleListen);
+
+// const server = http.createServer(app); // express app에서부터 http서버를 만든다.
+// const wss = new WebSocket.Server({server})  // http서버를 wss서버로 
 
 // const handleConnection = (soket) => {
 //     console.log(soket)
 //     // 이때의 소켓은 브라우저와 서버의 연결을 의미
 // }
 
-const sockets = [];
+// const sockets = [];
 
-wss.on("connection", (socket)=> {
-    sockets.push(socket);
-    socket['nickname'] = 'Anon'
-    console.log('connected to broswer');
-    socket.on('close', () => console.log('disconnected from browser'))
-    socket.on('message', msg => {
-        const message = JSON.parse(msg);
-        switch(message.type){
-            case "new_message":
-                sockets.forEach((aSocket) => aSocket.send(`${socket.nickname} : ${message.payload}`));
-                break;
-            case "nickname":
-                socket.nickname = message.payload;
-                break;
-        }
-    })
-})
+// wss.on("connection", (socket)=> {
+//     sockets.push(socket);
+//     socket['nickname'] = 'Anon'
+//     console.log('connected to broswer');
+//     socket.on('close', () => console.log('disconnected from browser'))
+//     socket.on('message', msg => {
+//         const message = JSON.parse(msg);
+//         switch(message.type){
+//             case "new_message":
+//                 sockets.forEach((aSocket) => aSocket.send(`${socket.nickname} : ${message.payload}`));
+//                 break;
+//             case "nickname":
+//                 socket.nickname = message.payload;
+//                 break;
+//         }
+//     })
+// })
 // 위의 경우 http와 wss를 합친 상태다.
 // 물론 위의 경우처럼 같은 서버에서 http와 wss 둘다 작동시킬 수도 있지만
 // 둘 중 하나만 사용해도 된다.
-server.listen(3000, handleListen); // app에서 리슨을 하지 않고 http server에서 리슨을 한다. 그리고 wss를 그 위에 만든 상태가 된다.
+// server.listen(3000, handleListen); // app에서 리슨을 하지 않고 http server에서 리슨을 한다. 그리고 wss를 그 위에 만든 상태가 된다.
 // 따라서 localhost는 동일한 포트에서 http, wss request 두 개를 처리할 수 있다.
 // 현재 http서버와 wss를 함께 만든 이유는 
 // view, static files, home, redirection을 사용해야 하기 때문임.
