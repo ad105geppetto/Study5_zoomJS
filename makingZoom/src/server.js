731f6e5d273ac7221d1a963dd3be47288003b0ea
geppetto <ad105geppetto@gmail.com>
2022-04-18 22:43:08 +0900

    update answer

--- makingZoom/src/server.js
+++ makingZoom/src/server.js
@@ -6,22 +6,24 @@ const port = 3000
 
 
 app.set("view engine", "pug");
-app.set("views", __dirname + "/views")
-app.use("/public", express.static(__dirname + "/public"))
-app.get('/', (_, res) => {res.render('home')})
-app.get('/*', (_, res) => {res.redirect('/')})
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get('/', (_, res) => {res.render('home')});
+app.get('/*', (_, res) => {res.redirect('/')});
 
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", socket => {
-  socket.on("join_room", (roomName, done) => {
+  socket.on("join_room", (roomName) => {
     socket.join(roomName)
-    done()
     socket.to(roomName).emit("welcome")
   })
   socket.on('offer', (offer, roomName) => {
     socket.to(roomName).emit("offer", offer)
+  })
+  socket.on('answer', (answer, roomName) => {
+    socket.to(roomName).emit('answer', answer);
   })
 })
 
